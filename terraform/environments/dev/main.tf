050cbb8fcc349978ce621ecc47183a385e2923d7
Jeff Shively <jshively@paloaltonetworks.com>
2024-06-26 09:59:49 -0400

    Changing vn to vnet

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vn" {
-  name                = "${var.user_name}-${var.role}-vn"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.user_name}-${var.role}-vnet"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = var.address_space
