34c2dc92f53c1d6a0ceea8cadc71ca351bd0751b
Jeff Shively <jshively@paloaltonetworks.com>
2024-06-26 12:51:10 -0400

    Creating subnets from CIDR

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -10,3 +10,31 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.address_space
   tags                = var.tags
 }
+
+locals {
+  subnets = tolist(cidrsubnets(var.address_space[0], 2, 2, 2, 2))
+  untrust_subnet = [local.subnets[0]]
+  trust_subnet = [local.subnets[1]]
+  mgmt_subnet = [local.subnets[3]]
+}
+
+resource "azurerm_subnet" "untrust" {
+  name = "untrust"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = local.untrust_subnet
+}
+
+resource "azurerm_subnet" "trust" {
+  name = "trust"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = local.trust_subnet
+}
+
+resource "azurerm_subnet" "mgmt" {
+  name = "mgmt"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = local.mgmt_subnet
+}
---
+++ terraform/environments/dev/outputs.tf
@@ -0,0 +1,15 @@
+output "subnets" {
+  value = local.subnets
+}
+
+output "untrust_subnet" {
+    value = local.untrust_subnet
+}
+
+output "trust_subnet" {
+    value = local.trust_subnet
+}
+
+output "mgmt_subnet" {
+    value = local.mgmt_subnet
+}
