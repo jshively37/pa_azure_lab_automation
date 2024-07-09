1c34b296b5a649ca2f4bc2bb2cbb8744c9909461
Jeff Shively <jshively@paloaltonetworks.com>
2024-07-09 14:03:43 -0400

    Creating static IP entries for each device

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,19 +1,3 @@
-# output "subnets" {
-#   value = local.subnets
-# }
-
-# output "untrust_subnet" {
-#   value = local.untrust_subnet
-# }
-
-# output "trust_subnet" {
-#   value = local.trust_subnet
-# }
-
-# output "mgmt_subnet" {
-#   value = local.mgmt_subnet
-# }
-
 output "mgmt_ip" {
   value = azurerm_public_ip.mgmt_public.ip_address
 }
